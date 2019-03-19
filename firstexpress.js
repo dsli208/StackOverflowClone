66bca1ece764ba41880e203422f13ed2b518869d
dsli208 <ds.li@outlook.com>
2019-03-19 17:08:05 -0400

    Add MongoDB code for verification in login and add verify part (first draft)

--- firstexpress.js
+++ firstexpress.js
@@ -5,6 +5,9 @@ const port = 80
 
 const nodemailer = require('nodemailer');
 var NodeSession = require('node-session');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(express.json());
 
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
@@ -73,14 +76,14 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/', (req, res) => res.send('App running'))
 
-app.post('/test', (req, res) => function(req, res) {
+app.post('/test', (req, res) => {
   console.log("Test endpoint routing");
   res.send("Test return value");
 })
 
 app.get('/adduser', (req, res) => res.send('GET for /adduser'))
 
-app.post('/adduser', (req, res) => function(req, res) {
+app.post('/adduser', (req, res) => {
   console.log("Add User POST Request");
   // Missing key cases
   if (req.body.username == null) {
@@ -99,6 +102,7 @@ app.post('/adduser', (req, res) => function(req, res) {
     // Insert into USERS Database
     var username = req.body.username;
     var password = req.body.password;
+    console.log(username + " " + password);
     console.log("Connecting to DB ...");
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
@@ -112,21 +116,24 @@ app.post('/adduser', (req, res) => function(req, res) {
     });
 
     // Send key to the given email
-    console.log("Sending key");
+    console.log("Generating key");
     var key = "abracadbra";
     var email = req.body.email;
+    console.log(email);
 
     //Step: 2 Setup message options
     var mailOptions = {
       from: 'someguy@cato.com',
-      to: 'myfriend@yahoo.com',
+      to: email,
       subject: 'Registration',
       text: 'Key is ' + key
     };
 
     //Step: 3 Send mail using created transport
+    console.log("Sending email");
     global.transporter.sendMail(mailOptions, function(error, info){
       if (error) {
+        console.log("Error message below");
         console.log(error);
       } else {
         console.log('Email sent: ' + info.response);
@@ -137,7 +144,48 @@ app.post('/adduser', (req, res) => function(req, res) {
   }
 })
 
-app.post('/login', (req, res) => function(req, res) {
+app.post('/verify', (req, res) => {
+  if (req.body.email == null) {
+    res.json({"status": "error", "error": "no email found"});
+  }
+  else if (req.body.key == null) {
+    res.json({"status": "error", "error": "no verification key found"});
+  }
+  // All forms filled
+  else {
+    var email = req.body.email;
+    var key = req.body.key;
+
+    var username = "", password = "";
+
+    // Find the user in the database, then make sure email matches the Key
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("mydb");
+
+      // Get user email and check it matches up with the key sent to that email
+      dbo.collection("users").findOne({"email": email}, function(err, result) {
+        if (err) res.json({"status": "error", "error": "Email not found"});
+        console.log(result.email);
+        if (result.key !== key) res.json({"status": "error", "error": "Email and key do not match up."});
+        else { username = result.username; password = result.password;}
+        db.close();
+      });
+
+      // Since we have verified the user, add them to the verified users colelction so that they can log in
+      dbo.collection("verified_users").insertOne({"username":username, "password":password}) {
+        if (err) throw err;
+        console.log("1 verified user added to VERIFIED USERS collection");
+        db.close();
+      }
+    });
+
+    // User from users database now verified and added to verified_users Database
+    res.json({"status": "OK"});
+  }
+})
+
+app.post('/login', (req, res) => {
   if (req.body.username == null) {
     res.json({"status": "error", "error": "no username found"});
   }
@@ -148,15 +196,27 @@ app.post('/login', (req, res) => function(req, res) {
     var username = req.body.username;
     var password = req.body.password;
 
-    // Determine that this user is verified
 
+
+    // Determine that this user is verified
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("mydb");
+      dbo.collection("verified_users").findOne({"username": username}, function(err, result) {
+        if (err) res.json({"status": "error", "error": "User is not verified"});
+        console.log(result.username);
+        if (result.password !== password) res.json({"status": "error", "error": "Username and password do not match up."});
+        db.close();
+      });
+    });
+    console.log("Entry found. Logging in.");
 
     // If verified, put them in the session
     req.session.put('Username', username);
   }
 })
 
-app.post('/logout', (req, res) => function(req, res) {
+app.post('/logout', (req, res) => {
   req.session.flush();
 
   res.json({"status": "OK"});
