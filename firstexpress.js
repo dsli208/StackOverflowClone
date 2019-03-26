b9bda5d23c6722bff890308cb748479f3b3b8898
dsli208 <ds.li@outlook.com>
2019-03-25 20:36:12 -0400

    Fix routing bug for search

--- firstexpress.js
+++ firstexpress.js
@@ -454,7 +454,7 @@ app.get('/questions/:id/answers', (req, res) => {
   })
 })
 
-app.post('/questions/search', (req, res) => {
+app.post('/search', (req, res) => {
   var timestamp = Date.now();
   var limit = 25;
   var accepted = false;
