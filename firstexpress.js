5521e50207363aba1c597f61d168f53835cefaf5
dsli208 <ds.li@outlook.com>
2019-04-09 21:20:30 -0400

    Minor session fix

--- firstexpress.js
+++ firstexpress.js
@@ -411,7 +411,7 @@ app.post('/questions/:id/answers/add', (req, res) => {
   // First, check that a user is logged in
   if (req.session['__attributes']['username'] == null && req.session.username == null) {
     console.log("No user logged in at POST 1 ");
-    if (glob_username == null) {
+    if (glob_session == null) {
       res.json({"status": "error", "error": "No user logged in"});
     }
     else {
