b0623508e1dc33ab6960aef296d18d5bdaab9483
dsli208 <ds.li@outlook.com>
2019-04-27 13:45:52 -0400

    Play around with debugs at this poitn

--- firstexpress.js
+++ firstexpress.js
@@ -420,9 +420,10 @@ app.post('/questions/add', (req, res) => {
             var not_error = true;
             for (var i = 0; i < req.body.media.length && not_error; i++) {
               var media_id = req.body.media[i];
+              console.log(media_id);
               sodb.collection("media").findOne({"mid": media_id}).then(function(e2, r2) {
                 if (e2) {
-                  console.log(e2); console.log(r2);
+                  console.log(r2);
                   console.log("Nonexistent media - ERROR");
                   retdict = {"status": "error", "error": "Media file does not exist for this ID - error"}; // file doesn't exist
                   //res.send(403, {"status": "error", "error": "Media file does not exist for this ID"});
